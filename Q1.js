59687ad99fd52645fb933308bf313f32115af2ad
Mahil Sonowal <155747136+mahilsonowal@users.noreply.github.com>
2024-01-21 15:59:48 +0530

    Update Q1.js

--- Q1.js
+++ Q1.js
@@ -1,18 +1,26 @@
-// Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz". //
+// Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". //
+// For numbers which are multiples of both three and five print "FizzBuzz". //
 
+let output = [];
 
-for(let i=1; i<=100; i++) {
+function numbers() {
+    for(let i=1; i<=100; i++) {
     if ((i%3===0) && (i%5===0)){
-        console.log("FizzBuzz");
+        output.push ("FizzBuzz");
     }
     else if (i%3===0){
-        console.log("Fizz");
+        output.push("Fizz");
     }
     else if (i%5===0) {
-        console.log("Buzz");
+         output.push("Buzz");
     }
 
     else {
-        console.log(i);
+         output.push(i);
+        }
     }
+    console.log(output);
 }
+numbers();
+
+
